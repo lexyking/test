d2802625f8a377f204cefb6c54b9d141f265f3e2
Joan Yassi <joan@tray.io>
2024-07-21 09:28:16 +0100

    added dashboard info

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from "../ui/dashboard/sidenav";
 
+export const experimental_ppr = true
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col d:flex-row md:overflow-hidden">
